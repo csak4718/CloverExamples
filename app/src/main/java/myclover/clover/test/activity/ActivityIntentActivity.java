76873d6b2c7d471109ce2576aaa9c55c7aaeec4a
De-Wei Kung <csak4718@gmail.com>
2016-05-27 17:32:14 -0700

    broadcast receiver works

--- app/src/main/java/myclover/clover/test/activity/ActivityIntentActivity.java
+++ app/src/main/java/myclover/clover/test/activity/ActivityIntentActivity.java
@@ -5,10 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.clover.sdk.v1.Intents;
+import com.clover.sdk.v3.apps.AppsIntent;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import myclover.clover.test.R;
+
 
 public class ActivityIntentActivity extends AppCompatActivity {
 
@@ -18,11 +20,14 @@ public class ActivityIntentActivity extends AppCompatActivity {
 //        intent.putExtra(Intents.EXTRA_ORDER_ID, "23P9X00NGHFWT");
 //        startActivity(intent);
 
+        
 //        2: doesn't work
         Intent intent = new Intent(Intents.ACTION_START_REGISTER);
 //        String customerJson = "{'id': '5QM8KCB2Y0E5G', 'emailAddresses': [{'id': '5QM8KCB2Y0E5G','emailAddress': 'csak4718@gmail.com'}]}";
         String customerJson = "{'id': '5QM8KCB2Y0E5G'}";
-        intent.putExtra(Intents.EXTRA_CUSTOMER_V3, customerJson);
+
+//        intent.putExtra(Intents.EXTRA_CUSTOMER_V3, customerJson);
+        intent.putExtra("com.clover.intent.extra.CUSTOMER", customerJson);
         startActivity(intent);
     }
 
--- app/src/main/java/myclover/clover/test/receiver/MyBroadcastReceiver.java
+++ app/src/main/java/myclover/clover/test/receiver/MyBroadcastReceiver.java
@@ -1,31 +1,47 @@
 package myclover.clover.test.receiver;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
-import android.widget.Toast;
+import android.support.v4.app.NotificationCompat;
 
 import com.clover.sdk.v1.Intents;
 
-import myclover.clover.test.activity.ReceiveNotifActivity;
+import myclover.clover.test.R;
+import myclover.clover.test.activity.MainActivity;
 
 /**
  * Created by dewei.kung on 5/26/16.
  */
 public class MyBroadcastReceiver extends BroadcastReceiver{
 
+    private void showNotification(Context context, String orderId) {
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
+                new Intent(context, MainActivity.class), 0);
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.drawable.test_icon)
+                        .setContentTitle("My notification")
+                        .setContentText(orderId);
+        mBuilder.setContentIntent(contentIntent);
+        mBuilder.setDefaults(Notification.DEFAULT_SOUND);
+        mBuilder.setAutoCancel(true);
+        NotificationManager mNotificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(1, mBuilder.build());
+
+    }
+
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
         if (action.equals(Intents.ACTION_ORDER_CREATED)) {
-            Log.d("MyBroadcastReceiver", "RECEIVE");
             String orderId = intent.getStringExtra(Intents.EXTRA_CLOVER_ORDER_ID);
-            Toast.makeText(context, orderId, Toast.LENGTH_LONG).show();
-
-            Intent myIntent = new Intent(context, ReceiveNotifActivity.class);
-            myIntent.putExtra(ReceiveNotifActivity.EXTRA_PAYLOAD, "My data");
-            context.startActivity(myIntent);
+            showNotification(context, orderId);
         }
     }
 }
